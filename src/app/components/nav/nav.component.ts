c41a8b9bffd0382a22ec26bcb877f66b98fa0e81
KaduSR <carlos847@gmail.com>
2024-04-03 14:37:28 -0300

    HTML e CSS para Login

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
 export class NavComponent implements OnInit {
   constructor(private router: Router) { }
   ngOnInit(): void {
-      this.router.navigate(['tecnicos'])
+      this.router.navigate(['login'])
   }
 
 }
