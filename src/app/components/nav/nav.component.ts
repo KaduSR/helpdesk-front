8c1fe53df5948b38374f4bfbae8e33e022befab2
venblaras <carlos847@gmail.com>
2024-04-05 06:57:55 -0300

    Nav component updated to include logout functionality
    The nav component HTML and TypeScript files have been updated to include a logout function. The logout function navigates the user to the login page, clears local storage, and displays a success toast message. Additionally, the AuthService class has been updated to include a logout method that clears local storage. The ToastrService has been imported in the nav component to display toast messages.

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthGuard } from './../../auth/auth.guard';
+import { AuthService } from './../../services/auth.service';
+import { Toast, ToastrService } from 'ngx-toastr';
 
 @Component({
-  selector: 'app-nav',
-  templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css'
+  selector: "app-nav",
+  templateUrl: "./nav.component.html",
+  styleUrl: "./nav.component.css",
 })
 export class NavComponent implements OnInit {
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+    private AuthService: AuthService,
+    private Toast: ToastrService
+  ) {}
   ngOnInit(): void {
-      this.router.navigate(['home'])
+    this.router.navigate(["home"]);
   }
 
+  logout() {
+    this.router.navigate(["login"]);
+    this.AuthService.logout();
+    this.Toast.info("Logout Successfully",'Logout', {timeOut:7000});
+
+  }
 }
