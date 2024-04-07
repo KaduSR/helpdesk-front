35de43b2340938dbad4a767e106123d7f08914e2
KaduSR <carlos847@gmail.com>
2024-04-07 16:06:59 -0300

    "Added create, update and delete methods to TecnicoService and implemented creation of new technician in ColaboradoresCreateComponent"
    This commit message describes the changes made in the code diff above. It mentions the addition of create, update, and delete methods to the TecnicoService and the implementation of the creation of a new technician in the ColaboradoresCreateComponent. The message is concise and accurately describes the changes made in the code.

--- src/app/components/tecnico/colaboradores-create/colaboradores-create.component.ts
+++ src/app/components/tecnico/colaboradores-create/colaboradores-create.component.ts
@@ -1,5 +1,9 @@
 import { Component } from "@angular/core";
 import { FormControl, Validators } from "@angular/forms";
+import { ToastrService } from "ngx-toastr";
+import { Tecnico } from "../../../models/tecnico";
+import { TecnicoService } from "../../../services/tecnico.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-colaboradores-create",
@@ -7,6 +11,15 @@ import { FormControl, Validators } from "@angular/forms";
   styleUrl: "./colaboradores-create.component.css",
 })
 export class ColaboradoresCreateComponent {
+  colaborador: Tecnico = {
+    id: "",
+    nome: "",
+    cpf: "",
+    email: "",
+    senha: "",
+    perfis: [],
+    dataCriacao: "",
+  };
   nome: FormControl = new FormControl(null, Validators.minLength(3));
 
   CPF: FormControl = new FormControl(null, Validators.required);
@@ -15,16 +28,43 @@ export class ColaboradoresCreateComponent {
 
   senha: FormControl = new FormControl(null, Validators.minLength(8));
 
-  constructor() {
-  }
+  constructor(private service: TecnicoService, 
+    private Toast: ToastrService,
+  private router: Router) {}
 
   ngOnInit() {}
+
+  create(): void {
+    this.service.create(this.colaborador).subscribe(
+      () => {
+        this.Toast.success("Colaborador criado com sucesso!", "Sucesso");
+        this.router.navigate(["colaboradores"]);
+      },
+      (logerror) => {
+        if (logerror.error.errors) {
+          logerror.error.errors.forEach((element) => {
+            this.Toast.error(element.message, "Erro!");
+          });
+        } else {
+          this.Toast.error(logerror.error.message, "Erro!");
+        }
+      }
+    );
+  }
+  addPerfil(perfil: any): void {
+    if (this.colaborador.perfis.includes(perfil)) {
+      this.colaborador.perfis.splice(
+        this.colaborador.perfis.indexOf(perfil),
+        1
+      );
+    } else {
+      this.colaborador.perfis.push(perfil);
+    }
+  }
 
   validaCampos(): boolean {
     return (
       this.nome.valid && this.CPF.valid && this.email.valid && this.senha.valid
     );
   }
-
- 
 }
--- src/app/services/tecnico.service.ts
+++ src/app/services/tecnico.service.ts
@@ -14,5 +14,15 @@ export class TecnicoService {
     findAll(): Observable<Tecnico[]> {
       return this.http.get<Tecnico[]>(`${API_CONFIG.baseURL}/tecnicos`);
     }
+    
+    create(tecnico: Tecnico): Observable<Tecnico> {
+      return this.http.post<Tecnico>(`${API_CONFIG.baseURL}/tecnicos`, tecnico);
+    }
 
-}
+    update(tecnico: Tecnico): Observable<Tecnico> {
+      return this.http.put<Tecnico>(`${API_CONFIG.baseURL}/tecnicos/${tecnico.id}`, tecnico);
+    }
+    delete(id: number): Observable<void> {
+      return this.http.delete<void>(`${API_CONFIG.baseURL}/tecnicos/${id}`);
+    }
+  }
