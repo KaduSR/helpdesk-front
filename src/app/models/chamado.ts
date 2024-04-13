2e5e377aabf24d94b73a1ccf2464338948a83ef8
venblaras <carlos847@gmail.com>
2024-04-13 11:55:37 -0300

    model chamado.ts criado

---
+++ src/app/models/chamado.ts
@@ -0,0 +1,13 @@
+export interface Chamado {
+  id?: any;
+  dataAbertura?: String;
+  dataFechamento?: String;
+  prioridade: string;
+  status: string;
+  titulo: String;
+  descricao: string;
+  tecnico: any;
+  cliente: any;
+  nomeCliente: String;
+  nomeTecnico: string;
+}
