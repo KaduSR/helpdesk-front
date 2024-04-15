5446b2b1b5f1d423122968d28b35c5bf5482a333
venblaras <carlos847@gmail.com>
2024-04-15 10:17:54 -0300

    "Update ChamadoListComponent to use MatTableDataSource and MatPaginator"
    This commit updates the ChamadoListComponent to use the MatTableDataSource and MatPaginator from '@angular/material' to display and paginate the

--- src/app/components/chamado/chamado-list/chamado-list.component.ts
+++ src/app/components/chamado/chamado-list/chamado-list.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Chamado } from '../../../models/chamado';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-chamado-list',
   templateUrl: './chamado-list.component.html',
-  styleUrl: './chamado-list.component.css'
+  styleUrl: './chamado-list.component.css',
 })
-export class ChamadoListComponent {
+export class ChamadoListComponent implements OnInit {
+  ELEMENT_DATA: Chamado[] = [];
 
+  displayedColumns: string[] = [
+    'position',
+    'titulo',
+    'cliente',
+    'dataAbertura',
+    'dataFechamento',
+    'prioridade',
+    'status',
+    'acoes',
+  ];
+  dataSource = new MatTableDataSource<Chamado>(this.ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor () { }
+  
+  ngOnInit(): void {}
 }
